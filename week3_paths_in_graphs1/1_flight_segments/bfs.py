bec3b7100f311da69b49fe6501df574d91c0304d
Sadeed <sadeed.hakimi@yahoo.com>
2020-09-29 21:18:02 -0700

    DFS for shortest path and bipartite graphs

--- week3_paths_in_graphs1/1_flight_segments/bfs.py
+++ week3_paths_in_graphs1/1_flight_segments/bfs.py
@@ -3,9 +3,24 @@
 import sys
 import queue
 
-def distance(adj, s, t):
+
+def distance(adj, S, t):
     #write your code here
-    return -1
+    dist = [-1] * len(adj)
+    dist[S] = 0
+
+    Q = queue.Queue()
+    Q.put(S)
+
+    while not Q.empty():
+        u = Q.get()
+        for v in adj[u]:
+            if dist[v] == -1:
+                dist[v] = dist[u] + 1
+                Q.put(v)
+
+    return dist[t]
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -19,3 +34,27 @@ if __name__ == '__main__':
         adj[b - 1].append(a - 1)
     s, t = data[2 * m] - 1, data[2 * m + 1] - 1
     print(distance(adj, s, t))
+
+'''
+Input:
+4 4
+1 2
+4 1
+2 3
+3 1
+2 4
+Output:
+2
+There is a unique shortest path between vertices 2 and 4 in this graph: 2 − 1 − 4.
+
+Input:
+5 4
+5 2
+1 3
+3 4
+1 4
+3 5
+Output:
+-1
+There is no path between vertices 3 and 5 in this graph.
+'''
--- week3_paths_in_graphs1/2_bipartite/bipartite.py
+++ week3_paths_in_graphs1/2_bipartite/bipartite.py
@@ -3,9 +3,36 @@
 import sys
 import queue
 
+
+def dfs(graph, src, color_list):
+    # colors: 1, 0       No color: -1
+    color_list[src] = 1
+    Q = list()
+    Q.append(src)
+
+    while Q:
+        u = Q.pop()
+        for v in graph[u]:
+            if color_list[v] == -1:
+                # assign 1 or 0
+                color_list[v] = 1 - color_list[u]
+                Q.append(v)
+            elif color_list[v] == color_list[u]:
+                return False
+    return True
+
+
 def bipartite(adj):
-    #write your code here
-    return -1
+    # two colors: 0, 1       No color: -1
+    color_list = [-1] * len(adj)
+
+    # loop over all vertices if there are multiple components in the graph
+    for src in range(len(adj)):
+        if color_list[src] == -1:
+            if not dfs(adj, src, color_list):
+                return 0
+    return 1
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -18,3 +45,41 @@ if __name__ == '__main__':
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
     print(bipartite(adj))
+
+'''
+Input:
+4 4
+1 2
+4 1
+2 3
+3 1
+Output:
+0
+This graph is not bipartite. To see this assume that the vertex 1 is colored white. Then the vertices 2
+and 3 should be colored black since the graph contains the edges {1, 2} and {1, 3}. But then the edge
+{2, 3} has both endpoints of the same color.
+
+Input:
+5 4
+5 2
+4 2
+3 4
+1 4
+Output:
+1
+This graph is bipartite: assign the vertices 4 and 5 the white color, assign all the remaining vertices
+the black color.
+
+Input:
+8 7
+5 2
+4 2
+3 4
+1 4
+6 7
+8 6
+7 8
+
+0
+There is two SCC (components). The 6-7-8 is not bipartite.
+'''
